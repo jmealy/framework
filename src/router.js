779fb8405a8d03d4d1a2aa7e2ec6ec4ae6680749
James <jamesmealy1@gmail.com>
2020-02-09 11:12:03 +0000

    fix: highscore calculation

--- src/router.js
+++ src/router.js
@@ -1,23 +1,32 @@
 const router = require('express').Router();
 
-const checkPalindrome = (word) => {
+const checkPalindrome = word => {
   const reversedWord = word.split('').reverse().join('');
   return word === reversedWord;
 }
 
 let highScores = [];
+let thresholdScore = 0;
+
+const addScore = score => {
+  highScores.sort((a, b) => a.points < b.points);
+  if (highScores.length < 5) {
+    highScores.push(score);
+  } else if (score.points > thresholdScore) {
+    highScores.pop();
+    highScores.push(score);
+  }
+  const lastIndex = highScores.length - 1;
+  thresholdScore = highScores[lastIndex].points;
+}
+
 
 router.get('/api/getScores', (req, res) => {
   // console.log(JSON.stringify(highscores));
-  highScores.sort( (a, b) => {
-    return a.points < b.points
-  });
-
-  const scorelength = highScores.length;
-
-  const topFive = highScores.slice(0, 5);
-  console.log(topFive)
-	res.hsend(highScores);
+  // highScores.sort( (a, b) => {
+  //   return a.points < b.points
+  // });
+  res.send(highScores);
 });
 
 router.post('/api/submitEntry', (req, res) => {
@@ -26,7 +35,8 @@ router.post('/api/submitEntry', (req, res) => {
   const isPalindrome = checkPalindrome(word);
 
   if (isPalindrome) {
-    highScores.push({ name: name, points: word.length});
+    // highScores.push({ name: name, points: word.length });
+    addScore({ name: name, points: word.length });
 
     res.send('word.length');
   } else {
