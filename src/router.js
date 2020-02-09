302b8c6109d5f0e88620c23df6350af340cda7f9
James <jamesmealy1@gmail.com>
2020-02-09 11:30:20 +0000

    clean up highscore calc

--- src/router.js
+++ src/router.js
@@ -6,19 +6,18 @@ const checkPalindrome = word => {
 }
 
 let highScores = [];
-let thresholdScore = 0;
+let thresholdScore;
 
 const addScore = score => {
   if (highScores.length < 5) {
     highScores.push(score);
-  } else if (score.points > thresholdScore) {
+  } else if (score.points > thresholdScore.points) {
     highScores.pop();
     highScores.push(score);
   }
 
   highScores.sort((a, b) => a.points < b.points);
-  const lastIndex = highScores.length - 1;
-  thresholdScore = highScores[lastIndex].points;
+  thresholdScore = highScores[highScores.length - 1];
 }
 
 
